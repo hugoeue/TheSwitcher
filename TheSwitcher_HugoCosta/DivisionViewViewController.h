1ebf0ac6d19562ddc638c921e6aa2409250e98a5
Hugo Costa <hugocosta@MacBook-Pro-de-Hugo.local>
2018-04-21 00:14:57 +0100

    first commit

---
+++ TheSwitcher_HugoCosta/DivisionViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  DivisionViewViewController.h
+//  TheSwitcher_HugoCosta
+//
+//  Created by Hugo Costa on 19/04/2018.
+//  Copyright © 2018 Hugo Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DivisionViewViewController : UIViewController
+
+@end
---
+++ TheSwitcher_HugoCosta/DivisionViewViewController.m
@@ -0,0 +1,37 @@
+//
+//  DivisionViewViewController.m
+//  TheSwitcher_HugoCosta
+//
+//  Created by Hugo Costa on 19/04/2018.
+//  Copyright © 2018 Hugo Costa. All rights reserved.
+//
+
+#import "DivisionViewViewController.h"
+
+@interface DivisionViewViewController ()
+
+@end
+
+@implementation DivisionViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
